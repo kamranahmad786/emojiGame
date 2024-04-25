9c0b3df1693d5792f390ae88d50c7f438e86797e
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-04-25 16:18:19 +0530

    Update index.js

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -58,7 +58,7 @@ class EmojiGame extends Component {
   clickEmoji = id => {
     const {emojisList} = this.props
     const {clickedEmojisList} = this.state
-    const isEmojiPresent = clickedEmojisList.include(id)
+    const isEmojiPresent = clickedEmojisList.includes(id)
     const clickedEmojisLength = clickedEmojisList.length
 
     if (isEmojiPresent) {
